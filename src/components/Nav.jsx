38c0b8826e92934b1ca37ff3369df697eccbec71
Harsh D. Rao <harshrao26>
2025-03-28 22:05:54 +0530

    UI UPDATED

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -61,7 +61,7 @@ const NavbarWithCountdown = () => {
 
       {/* Modal */}
       {showModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <div className="fixed inset-0 bg-[#000000b5] bg-opacity-50 flex justify-center items-center z-50">
           <div className="bg-gray-300 p-8 rounded-lg w-full max-w-md relative">
             <button
               onClick={() => setShowModal(false)}
